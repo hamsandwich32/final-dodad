f3f0015a95766a6a1f2367d23c0da60d347efb5f
hannahbanana <112023861+hamsandwich32@users.noreply.github.com>
2023-04-24 20:44:09 +0000

    text added

---
+++ Intro.py
@@ -0,0 +1,7 @@
+def intro():
+	print("You wake up to your alarm blaring, you realize that it's been going off for the last five minutes.")
+	print("You hit the alarm expecting it to turn off. It yells at you to get up.")
+	print("You get up and start to look around, you look at the clock closer, you're 15 minutes late to your magic training class!")
+	print("You start to frantically run around your room and look for your things. You can't find your potion bag, your wand, or the food to feed your snail.")
+	print("You grab your bag from the night stand.")
+	print("")
