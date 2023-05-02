3b2e294e1907d129ba790db5402c279b6666c5a9
hannahbanana <112023861+hamsandwich32@users.noreply.github.com>
2023-05-02 18:22:25 +0000

    cant figure out this loop

--- Intro.py
+++ Intro.py
@@ -5,24 +5,29 @@ def intro_part():
 	print("You start to frantically run around your room and look for your things. You can't find your potion bag, your wand, or the food to feed your snail.\n")
 	print("You look around once more and see your bag on your nightstand, you grab it.\n")
 	print("You're still missing a lot of your stuff, where do you look?\n")
-	print("1 - check under your bed")
-	print("2 - check in the drawers of the nightstand")
+	
 	while True:
+		print("1 - check under your bed")
+		print("2 - check in the drawers of the nightstand")
 		bn = input()
 		try:
 			if int(bn) == 1:
 				print("You look under the bed, you feel something pointy. You reach for it and pull it from under the bed. It's a unicorn horn. Ugh, useless.\n")
-				print("Pick an option to chose from.")
 				break
-			if int(bn) == 2:
-				print("You open the top drawer of your nightstand you find a brown leather bag of salt. What could this possibly be used for?\n")
-				print("")
-				print("You look at the bag of salt, it has a picture of a snail on it. Huh.")
-				print("You look around your room you see:")
-				break 
-			else:
-				print("Pick an option to chose from.")
 		except:
 			print("Value must be a whole number between 1 and 2.")
+	while True: 
+			bn = input()
+			try:
+				if int(bn) == 2:
+					print("You open the top drawer of your nightstand you find a brown leather bag of salt. What could this possibly be used for?\n")
+					print("")
+					print("You look at the bag of salt, it has a picture of a snail on it. Huh.")
+					print("You look around your room you see:")
+					break 
+				else:
+					print("Pick an option to chose from.")
+			except:
+				print("Value must be a whole number between 1 and 2.")
 
 
