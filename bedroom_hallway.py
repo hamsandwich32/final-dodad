2871f7d3e138ed2818620790b30e30142f33f3f9
hannahbanana <112023861+hamsandwich32@users.noreply.github.com>
2023-05-02 19:10:15 +0000

    #ed out menu items and while statement

--- bedroom_hallway.py
+++ bedroom_hallway.py
@@ -4,12 +4,12 @@ def hallway1():
 	print("The hallway is dimly lit from the floating candles that span the entire hallway.")
 	print("You see a door and realize that's the etrance to your potion room, you remember that you left a potion you need by the culdron.")
 	print("You make your way down the hallway. The hallway seems to twist and turn as you start to go down it.")
-	print("Doors start to appear around you, which one are you supposed to go down?")
-	while True:
-		print("1 - open the door next to you")
-		print("2 - keep walking down the hall until the end")
-		attempt_1 = input()
-		try:
-			if int(attempt_1) == 1:
-				
+	print("Doors start to appear around you, which one are you supposed to go down?\n")
+	print("1 - open the door next to you")
+	print("2 - keep walking down the hall until the end")
+	#while True:
+	#	attempt_1 = input()
+	#	try:
+	#		if int(attempt_1) == 1:
+
 
