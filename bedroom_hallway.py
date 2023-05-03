90528e6efec5551230cfd0ac0560ce390d335ab6
hannahbanana <112023861+hamsandwich32@users.noreply.github.com>
2023-05-03 18:27:16 +0000

    still working

--- bedroom_hallway.py
+++ bedroom_hallway.py
@@ -5,20 +5,34 @@ def hallway1():
 	print("You see a door and realize that's the etrance to your potion room, you remember that you left a potion you need by the culdron.")
 	print("You make your way down the hallway. The hallway seems to twist and turn as you start to go down it.")
 	print("Doors start to appear around you, which one are you supposed to go down?\n")
+
 	print("1 - open the door next to you")
 	print("2 - keep walking down the hall until the end")
-	attempt_1 = input()
-	while attempt_1 == 1 or 2:
-		try:
-			
-			if int(attempt_1) == 1:
-				print("You reach for the door ahead of you, your arm goes through the door. It's not this one.")
-				print("Try again.")
-				break
-			if int(attempt_1) == 2:
-				print("The hallway keeps spinning around you, you finally reach the door and try to open it. It's locked.")
-		except:
+	#attempt_1 = input()
+	#while attempt_1 == 1 or 2:
+		#try:
+	while True:
+		attempt_1 = input()
+		if attempt_1 == 1:
+			print("You reach for the door ahead of you, your arm goes through the door. It's not this one.\n")
 			print("Try again.")
+				
+		elif attempt_1 == 2:
+			print("The hallway keeps spinning around you, you finally reach the door and try to open it. It's locked.\n")
+		else:
+			print("Try again.")
+			#False
+
+	print("")
+	print("You walk around the hallway trying to find the key. You realize you probably shouldn't have a distorting hallway anymore.\n")
+	print("As you look around for the key, you see that the hallway diverges to the right and the left.")
+
+	print("1 - go down the hallway to the left")
+	print("2 - go down the hallway to the right")
+	print("3 - keep looking for the key in the distorting hallway")
+	#attempt_2 = input()
+	#while attempt_2 == 1 or 2 or 3
+
 	
 
 
--- main.py
+++ main.py
@@ -5,5 +5,3 @@ from bedroom_hallway import hallway1
 hallway1()
 items = ["potion bag","snail food","broomstick","wand","potion","leash","key","crystal"]
 locations = ["bed","table","culdron","cabnet", "shelve","Pantry"]
-#menu_1_items = ["Ladder","Bucket","Chair","Mattress","Carpet"]
-#menu_2_items = ["Fence", "Gate", "Bike", "Rock", "Pot"]
