fdd1111ffc454a1c47dcf907aea74e7015194726
hannahbanana <112023861+hamsandwich32@users.noreply.github.com>
2023-05-04 03:38:59 +0000

    some break statement needs to be added brain mush

---
+++ potion_room.py
@@ -0,0 +1,54 @@
+def potion_weirdness():
+	print("After unlocking the door you walk into the potion room.")
+	print("Now you need to find that potion you were missing.")
+	print("You have no idea where to start, potions line the entire room with only a narrow passage through them. Ugh. You are so gonna get in trouble with your teacher.")
+	MENU = '''
+	1 - go forward
+	2 - go back'''
+	attempt_1 = 'a':
+	while attempt_1 == 1 or 2:
+		print(MENU)
+		attempt_1 = input("What's your choice?\n")
+		if attempt_1 == '1':
+			print("You walk forward and the path diverges into . Where do you three paths where do you go from here?\n")
+			MENU = '''
+			1 - go right
+			2 - go left
+			3 - go straight'''
+			print(MENU)
+
+
+			if attempt_1 == '1':
+				print("You're greeted with a profoundly large door. Why do you have this? You try the knob and it licks you.\
+				You need to find a sacrifice to feed it.\n")
+				print(MENU)
+			if attempt_1 == '2':
+				MENU = '''
+				1 - go right
+				2 - go left'''
+				print("You walk to the end of the hallway and it diverges to the left and the right.\n")
+				print("You've been fooled once again by your own home. You really need to stop making things hard on yourself.\n")
+				print(MENU)
+
+
+				if attempt_1 == '1':
+					MENU = '''
+					1 - go left'''
+					print("You walk to the end of the hallway. You find a small sacrifice for the doorknob.\n")
+					print(MENU)
+				if attempt_1 == '2':
+					MENU = '''
+					1 - go right'''
+					print("You walk to the end of the hallway. You find the potion by the culdron, it smells weird. What is this again?\n")
+					print(MENU)
+			if attempt_1 == '3':
+				print("You walk down the hallway. Pictures of the witches who have owned the house before you hang.\
+				Why can't you be like them? Why must you be late to all of your training classes\n")
+				print("You slowly walk past all of the portraits, you just want to make them proud.\n")
+				print("You reach the door end of the hallway, you reach out and twist the handle. It opens for you, you walk through and are greeted with your bee. ")
+				break
+
+
+		if attempt_1 == '2':
+			print("You are back at the funky hallway from alice and wonderland. Don't go back.")
+			print(MENU)
