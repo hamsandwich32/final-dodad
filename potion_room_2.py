abc5cb5acfdd9a9e8a746f356bc0d94bf3efa901
hannahbanana <112023861+hamsandwich32@users.noreply.github.com>
2023-05-10 21:57:00 +0000

    I GOT IT TO WORK OMG

--- potion_room_2.py
+++ potion_room_2.py
@@ -14,11 +14,11 @@ def potion_weirdness():
 			print("Try Again.")
 			print(MENU)
 		elif attempt_1 == '1':
-			print("You walk forward and the path diverges into. Where do you three paths where do you go from here?\n")
 			MENU = '''
-			1 - go right
-			2 - go left
-			3 - go straight'''
+	1 - go right
+	2 - go left
+	3 - go straight'''
+			print("You walk forward and the path diverges into. Where do you three paths where do you go from here?\n")
 			print(MENU)
 			
 			attempt_2 = 'a'
@@ -31,8 +31,8 @@ def potion_weirdness():
 				elif attempt_2 == '2':
 				#menu not right 2 should be go back not left, or seperate option
 					MENU = '''
-					1 - go right
-					2 - go to potion hallway'''
+	1 - go right
+	2 - go to potion hallway'''
 					#print("You walk to the end of the hallway and it diverges to the left and the right.\n")
 					print("You've been fooled once again by your own home. You really need to stop making things hard on yourself.\n")
 					print(MENU)
@@ -42,21 +42,27 @@ def potion_weirdness():
 						attempt_3 = input("\nWhat's your choice?\n")
 						if attempt_3 == '1':
 							MENU = '''
-							2 - go back to potion room hallway'''
+	2 - go back to potion room hallway'''
 					#add in as item save to inventory
 							print("You walk to the end of the hallway. You find a large bookcase. You start to look around and you find a small sacrifice for the doorknob.\n")
 							print(MENU)
-							break
+							#break
 						
 						elif attempt_3 == '2':
-						
+							print("You walk forward and the path diverges into. Where do you three paths where do you go from here?\n")
+							MENU = '''
+	1 - go right
+	2 - go left
+	3 - go straight'''
+							print(MENU)
+							break
 						
 				
 						#if attempt_3 == '2':
                             #GO BACK TO ATTEMPT 1
-				elif attempt_1 == '3':
+				elif attempt_2 == '3':
 					print("You walk down the hallway. Pictures of the witches who have owned the house before you hang. Why can't you be like them? Why must you be late to all of your training classes\n")
 					print("You slowly walk past all of the portraits, you just want to make them proud.\n")
 					print("You reach the door end of the hallway, you reach out and twist the handle. It opens for you, you walk through and are greeted with your giant bee.")
-					break
+					
 									
